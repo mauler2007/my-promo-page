9942ecea60d7092097a7bd210b1e35dc51a150f3
mauler2007 <62986193+mauler2007@users.noreply.github.com>
2020-08-05 09:57:04 +0300

    added last changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 $(function(){
 
-
-
-  
+    $('.header__menu-btn').on('click', function(e){
+       $('.header__menu ul').slideToggle();
+       $(this).toggleClass('menu-btn__active')
+       
+        // e.preventDefault;
+        // $(this).toggleClass('menu-btn__active')
+    });
+    
 });
